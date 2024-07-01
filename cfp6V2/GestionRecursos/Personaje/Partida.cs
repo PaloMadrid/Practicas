715ec8b76d5a1d49aa41eb2661c9a80d78eade1c
PaloMadrid <paloma.madrid@bue.edu.ar>
2024-07-01 16:18:54 -0300

    agregar juego gestion

---
+++ cfp6V2/GestionRecursos/Personaje/Partida.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mis_librerias
+{
+    internal class Partida
+    {
+        string nombre;
+        int dias;
+
+        public Partida(string nombre, int dias)
+        {
+            this.nombre = nombre;
+            this.dias = dias;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Dias { get => dias; set => dias = value; }
+
+        
+    }
+}
---
+++ cfp6V2/GestionRecursos/Personaje/Personaje.cs
@@ -0,0 +1,47 @@
+﻿namespace mis_librerias
+{
+    public class Personaje
+    {
+
+        string nombre;
+        int suerte;
+        int vida;
+        int hambre;
+        string salud;
+
+        public Personaje(string nombre, int suerte)
+        {
+            this.nombre = nombre;
+            this.suerte = suerte;
+            this.vida = 100;
+            this.hambre = 10;
+            this.salud = "Bueno";
+        }
+
+        public string Nombre { get => nombre;  }
+        public int Suerte { get => suerte; set => suerte = value; }
+        public int Vida { get => vida; set => vida = value; }
+        public string Salud { get => salud; set => salud = value; }
+
+
+        public List<Personaje> ListaDePersonajes()
+        {
+            List <Personaje> personajes = new List<Personaje>()
+            {
+                new Personaje ("Juan",9),
+                new Personaje ("Favalli",8),
+                new Personaje ( "Elena" ,9), 
+                new Personaje("Lucas",7),
+                new Personaje("Polski",6)
+            };
+
+            return personajes;  
+        }
+
+        //public void ConsumirHambre()
+        //{
+        //    if
+
+        //}
+    }
+}
